140a667b34b106a483d96f69f506ec46b92df065
Michael Callaghan <michael.d.callaghan@disney.com>
2015-04-21 12:45:26 -0400

    Got a basic ngCordova Device plugin working.

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,50 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+(function () {
+    angular.module('starter', ['ionic', 'ngCordova'])
+        .controller('HomeController', ['$scope', '$cordovaDevice', HomeController])
+        .run(startup)
+        .config(config);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    function HomeController($scope, $cordovaDevice) {
+        var vm = this;
+
+        document.addEventListener('deviceready', function () {
+            if ($cordovaDevice) {
+                vm.cordova = $cordovaDevice.getCordova();
+                vm.model = $cordovaDevice.getModel();
+                vm.platform = $cordovaDevice.getPlatform();
+                vm.uuid = $cordovaDevice.getUUID();
+                vm.version = $cordovaDevice.getVersion();
+                $scope.$apply();
+            } else {
+                alert('No device support!');
+            }
+        });
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
+
+    function startup($ionicPlatform) {
+        $ionicPlatform.ready(function ($cordovaDevice) {
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
     }
-  });
-})
+
+    function config($stateProvider) {
+        console.log('config!');
+
+        $stateProvider.state('index', {
+            url: '',
+            templateUrl: 'templates/home.html',
+            controller: 'HomeController as vm'
+        });
+    }
+})();
